a378c0fb4d87dec7b4e8e7b6ef74cd4fff82a70b
Ingo Schommer <ingo@silverstripe.com>
2016-09-17 20:18:37 +1200

    Controller request/response

--- src/Controller.php
+++ src/Controller.php
@@ -4,29 +4,54 @@ namespace SilverStripe\GraphQL;
 
 use SilverStripe\Control\Controller as BaseController;
 use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Core\Config\Config;
 
 class Controller extends BaseController
 {
 
+    /**
+     * @var Manager
+     */
+    protected $manager;
+
     public function index(HTTPRequest $request) {
+        $query = $request->getVar('query');
+        $params = $request->getVar('params');
+
+        if(is_string($params)) {
+            $params = json_decode($params, true);
+        }
+
         $manager = $this->getManager();
+        $response = $manager->query($query, $params);
 
-        $schema = $manager->getSchema();
-
-
+        return (new HTTPResponse(json_encode($response)))
+            ->addHeader('Content-Type', 'text/json');
     }
 
     /**
      * @return Manager
      */
-    protected function getManager() {
+    public function getManager() {
+        if($this->manager) {
+            return $this->manager;
+        }
+
         // Get a service rather than an instance (to allow procedural configuration)
         $config = Config::inst()->get('SilverStripe\GraphQL', 'schema');
         $manager = Injector::inst()->create(Manager::class, $config);
 
         return $manager;
+    }
+
+    /**
+     * @param Manager $manager
+     */
+    public function setManager($manager)
+    {
+        $this->manager = $manager;
     }
 
 }
--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -2,15 +2,28 @@
 
 namespace SilverStripe\Tests\GraphQL;
 
+use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\GraphQL\Manager;
 use SilverStripe\GraphQL\Controller;
 use SilverStripe\GraphQL\Tests\Fake\TypeCreatorFake;
+use SilverStripe\GraphQL\Tests\Fake\QueryCreatorFake;
 use SilverStripe\Core\Config\Config;
 use ReflectionClass;
 
 class ControllerTest extends SapphireTest
 {
+    
+    public function testIndex()
+    {
+        $controller = new Controller();
+        $manager = new Manager();
+        $manager->addType(TypeCreatorFake::class, 'mytype');
+        $manager->addQuery(QueryCreatorFake::class, 'myquery');
+        $controller->setManager($manager);
+        $response = $controller->index(new HTTPRequest('GET', ''));
+        $this->assertFalse($response->isError());
+    }
 
     public function testGetGetManagerPopulatesFromConfig()
     {
