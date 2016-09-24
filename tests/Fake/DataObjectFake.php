1f1f93188ca7759ef334de6652c1678e9c6bef9e
Ingo Schommer <ingo@silverstripe.com>
2016-09-24 18:30:43 +1200

    Refactored field resolvers
    Needed a "setter" functionality, and in fact it has little to do with actual "resolving" in GraphQL sense

--- tests/Fake/DataObjectFake.php
+++ tests/Fake/DataObjectFake.php
@@ -7,6 +7,14 @@ use SilverStripe\ORM\DataObject;
 
 class DataObjectFake extends DataObject implements TestOnly
 {
+    private static $db = [
+        'MyField' => 'Varchar'
+    ];
+
+    public $customSetterFieldResult;
+
+    public $customSetterMethodResult;
+
     public function getCustomGetter()
     {
         return 'customGetterValue';
@@ -15,5 +23,15 @@ class DataObjectFake extends DataObject implements TestOnly
     public function customMethod()
     {
         return 'customMethodValue';
+    }
+
+    public function setCustomSetterField($val)
+    {
+        $this->customSetterFieldResult = $val;
+    }
+
+    public function customSetterMethod($val)
+    {
+        $this->customSetterMethodResult = $val;
     }
 }
---
+++ tests/Util/CaseInsensitiveFieldAccessorTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Chillu\GraphQL\Tests\Util;
+
+use Chillu\GraphQL\Util\CaseInsensitiveFieldAccessor;
+use Chillu\GraphQL\Tests\DataObjectFake;
+use SilverStripe\Dev\SapphireTest;
+
+class CaseInsensitiveFieldAccessorTest extends SapphireTest
+{
+
+    public function testGetValueWithOriginalCasing()
+    {
+        $fake = new DataObjectFake([
+            'MyField' => 'myValue'
+        ]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $this->assertEquals('myValue', $mapper->getValue($fake, 'MyField'));
+    }
+
+    public function testGetValueWithDifferentCasing()
+    {
+        $fake = new DataObjectFake([
+            'MyField' => 'myValue'
+        ]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $this->assertEquals('myValue', $mapper->getValue($fake, 'myfield'));
+    }
+
+    public function testGetValueWithCustomGetter()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $this->assertEquals('customGetterValue', $mapper->getValue($fake, 'customGetter'));
+    }
+
+    public function testGetValueWithMethod()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $this->assertEquals('customMethodValue', $mapper->getValue($fake, 'customMethod'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetValueWithUnknownFieldThrowsException()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->getValue($fake, 'unknownField');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testGetValueWithCustomOpts()
+    {
+        $fake = new DataObjectFake([
+            'MyField' => 'myValue'
+        ]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $opts = [
+            // only check for methods
+            CaseInsensitiveFieldAccessor::HAS_FIELD => false,
+            CaseInsensitiveFieldAccessor::DATAOBJECT => false,
+        ];
+        $mapper->getValue($fake, 'MyField', $opts);
+    }
+
+    public function testSetValueWithOriginalCasing()
+    {
+        $fake = new DataObjectFake([
+            'MyField' => 'myValue'
+        ]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->setValue($fake, 'MyField', 'myNewValue');
+        $this->assertEquals('myNewValue', $fake->MyField);
+    }
+
+    public function testSetValueWithDifferentCasing()
+    {
+        $fake = new DataObjectFake([
+            'MyField' => 'myValue'
+        ]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->setValue($fake, 'myfield', 'myNewValue');
+        $this->assertEquals('myNewValue', $fake->MyField);
+    }
+
+    public function testSetValueWithCustomGetter()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->setValue($fake, 'customsetterfield', 'myNewValue');
+        $this->assertEquals('myNewValue', $fake->customSetterFieldResult);
+    }
+
+    public function testSetValueWithMethod()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->setValue($fake, 'customsettermethod', 'myNewValue');
+        $this->assertEquals('myNewValue', $fake->customSetterMethodResult);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetValueWithUnknownFieldThrowsException()
+    {
+        $fake = new DataObjectFake([]);
+        $mapper = new CaseInsensitiveFieldAccessor();
+        $mapper->setValue($fake, 'unknownField', true);
+    }
+}
