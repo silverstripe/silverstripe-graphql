566623cd96016eb5aae2c86c3b4e1e78e5f8177a
Uncle Cheese <unclecheese@leftandmain.com>
2017-01-10 12:04:27 +1300

    Bugfix: missing use statement

--- src/Scaffolding/Scaffolders/CRUD/Delete.php
+++ src/Scaffolding/Scaffolders/CRUD/Delete.php
@@ -7,6 +7,7 @@ use SilverStripe\GraphQL\Scaffolding\Traits\DataObjectTypeTrait;
 use SilverStripe\ORM\DataList;
 use GraphQL\Type\Definition\Type;
 use SilverStripe\GraphQL\Scaffolding\Interfaces\CRUDInterface;
+use SilverStripe\GraphQL\Scaffolding\Scaffolders\GraphQLScaffolder;
 use Exception;
 
 /**
