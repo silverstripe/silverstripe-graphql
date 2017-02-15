76bb8e8cf4c5fead38e37d2956bd88d6b4fdec7e
Aaron Carlino <unclecheese@leftandmain.com>
2017-02-15 23:07:24 +1300

    Force $variables to cast as array
    GraphiQL passes variables as empty string and `GraphQL\Validator\Rules\QueryComplexity::setRawVariableValues()` chokes on a non-array value.

--- src/Controller.php
+++ src/Controller.php
@@ -63,10 +63,10 @@ class Controller extends BaseController
             $rawBody = $request->getBody();
             $data = json_decode($rawBody ?: '', true);
             $query = isset($data['query']) ? $data['query'] : null;
-            $variables = isset($data['variables']) ? $data['variables'] : null;
+            $variables = isset($data['variables']) ? (array) $data['variables'] : null;
         } else {
             $query = $request->requestVar('query');
-            $variables = json_decode($request->requestVar('variables'), true);
+            $variables = json_decode((array) $request->requestVar('variables'), true);
         }
 
         $this->setManager($manager = $this->getManager());
