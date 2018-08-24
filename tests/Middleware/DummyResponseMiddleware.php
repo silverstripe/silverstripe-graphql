2b335b4239946f9a6fb1d525452cf1fe6d22a9ce
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2018-08-24 14:32:59 +1200

    ENHANCEMENT Proof of concept of cached graphql queries (#166)
    * ENHANCEMENT Allow graphql response middleware to be applied
    FIxes #164 allows caching to be applied as user middleware
    
    * Add $schema and $context to middleware
    Add middleware tests

---
+++ tests/Middleware/DummyResponseMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SilverStripe\GraphQL\Tests\Middleware;
+
+use GraphQL\Schema;
+use SilverStripe\GraphQL\Middleware\QueryMiddleware;
+
+class DummyResponseMiddleware implements QueryMiddleware
+{
+    public function process(Schema $schema, $query, $context, $params, callable $next)
+    {
+        return ['result' => "It was me, {$params['name']}!"];
+    }
+}
