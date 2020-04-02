e3d6fbd7173dccd98b1d79942d0126d37931ad67
Aaron Carlino <unclecheese@leftandmain.com>
2020-04-03 11:06:57 +1300

    Add type hint
    Co-Authored-By: Daniel Hensby <dhensby@users.noreply.github.com>

--- tests/Scaffolding/StaticSchemaTest.php
+++ tests/Scaffolding/StaticSchemaTest.php
@@ -15,7 +15,7 @@ use SilverStripe\GraphQL\Tests\Fake\FakeSiteTree;
 
 class StaticSchemaTest extends SapphireTest
 {
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         StaticSchema::reset();
