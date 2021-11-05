a56a484fb534aaa11887b29c53d0fc8a2cad0ab7
Aaron Carlino <unclecheese@leftandmain.com>
2021-11-05 16:20:43 +1300

    New exception when NullDatabase is used

--- src/Dev/Build.php
+++ src/Dev/Build.php
@@ -7,11 +7,13 @@ use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Dev\DebugView;
+use SilverStripe\GraphQL\Schema\DataObject\FieldAccessor;
 use SilverStripe\GraphQL\Schema\Exception\EmptySchemaException;
 use SilverStripe\GraphQL\Schema\Exception\SchemaBuilderException;
 use SilverStripe\GraphQL\Schema\Exception\SchemaNotFoundException;
 use SilverStripe\GraphQL\Schema\Schema;
 use SilverStripe\GraphQL\Schema\SchemaBuilder;
+use SilverStripe\ORM\Connect\NullDatabaseException;
 
 class Build extends Controller
 {
@@ -62,11 +64,39 @@ class Build extends Controller
             Benchmark::start('build-schema-' . $key);
             Schema::message(sprintf('--- Building schema "%s" ---', $key));
             $builder = SchemaBuilder::singleton();
-            $schema = $builder->boot($key);
             try {
-                $builder->build($schema, $clear);
-            } catch (EmptySchemaException $e) {
-                Schema::message('Schema ' . $key . ' is empty. Skipping.');
+                $schema = $builder->boot($key);
+                try {
+                    $builder->build($schema, $clear);
+                } catch (EmptySchemaException $e) {
+                    Schema::message('Schema ' . $key . ' is empty. Skipping.');
+                }
+            } catch (NullDatabaseException $e) {
+                $candidate = null;
+                foreach ($e->getTrace() as $item) {
+                    $class = $item['class'] ?? null;
+                    $function = $item['function'] ?? null;
+                    // This is the only known path to a database query, so we'll offer some help here.
+                    if ($class === FieldAccessor::class && $function === 'accessField') {
+                        $candidate = $item;
+                        break;
+                    }
+                }
+                Schema::message("
+                    Your schema configuration requires access to the database. This can happen
+                    when you add fields that require type introspection (i.e. custom getters).
+                    It is recommended that you specify an explicit type when adding custom getters
+                    to your schema."
+                );
+                if ($candidate) {
+                    Schema::message(sprintf("
+                    This most likely happened when you tried to add the field '%s' to '%s'",
+                        $candidate['args'][1],
+                        get_class($candidate['args'][0])
+                    ));
+                }
+
+                throw $e;
             }
 
             Schema::message(
